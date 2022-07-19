48b3c492e4eb7bb5fec9731eb85fd6122df21a3d
Gabrielly Bailon <gabriellybailontur@gmail.com>
2022-07-19 00:06:31 -0300

    Model de task, migrations e criação de superuser

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
+from telnetlib import STATUS
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+
+    STATUS =(
+        ('fazendo', 'Fazendo'),
+        ('pronto', 'Pronto'),
+    )
+
+    title = models.CharField(max_length=255)
+    descricao = models.TextField()
+    done = models.CharField(
+        max_length=7,
+        choices=STATUS
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
